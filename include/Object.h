051c771048856d3e29752b98f6da0313b25aa6ae
Lingjie Fan <1072217465@qq.com>
2022-11-23 11:20:36 +0800

    #1 include and lib are added

---
+++ include/Object.h
@@ -0,0 +1,20 @@
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#define OBJECT &Object_Class
+
+typedef struct _Class Class;
+typedef struct _IObject IObject;
+typedef struct _Object Object;
+
+extern Class Object_Class;
+
+extern Object* Object_New(Object* this);
+
+extern Object* Object_Del(Object* this);
+
+extern int Object_Copy(Object* this, void* object);
+
+extern int Object_Equal(Object* this, void* object);
+
+#endif /*_OBJECT_H_*/
---
+++ include/ObjectC.h
@@ -0,0 +1,9 @@
+#ifndef _OBJECTC_H_
+#define _OBJECTC_H_
+
+#include "Class.h"
+#include "IObject.h"
+#include "Object.h"
+#include "Basic.h"
+
+#endif /*_OBJECTC_H_*/
